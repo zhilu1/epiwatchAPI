82142b84ef4246656f57236f2b255962df596e15
zhilu1 <z5096599@student.unsw.edu.au>
2019-03-12 16:13:30 +1100

    change man & settings

--- seng3011/settings.py
+++ seng3011/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = '/opt/SENG3011_Null/PHASE_1/API_SourceCode/static/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "sfiles"), )
